7785d97cf3edc7d5c80656ba13d1b7ed831b0c0d
Denisa <denisa.loseva@gmail.com>
2022-11-03 12:19:16 +0200

    Updated hw*

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,16 +1,21 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.lang.model.element.Element;
 import javax.swing.*;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FirstSeleniumTest {
     private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
+    private final By COUNTRIES = By.xpath(".//a[contains(@href, 'https://www.discovercars.com')]");
+    private final By TITLE = By.xpath(".//h1[contains(@class, 'home-h1')]");
 
     @Test
     public void openHomePageCheck() {
@@ -24,5 +29,14 @@ public class FirstSeleniumTest {
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
         browser.findElement(ACCEPT_COOKIES_BTN).click();
 
+        List<WebElement> countries = browser.findElements(COUNTRIES);
+        wait.until(ExpectedConditions.elementToBeClickable(countries.get(251)));
+        countries.get(251).click();
+
+        browser.findElement(TITLE);
+        System.out.println(browser.findElement(TITLE).getText());
+
+        browser.quit();
     }
 }
+
