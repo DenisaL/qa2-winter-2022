3acaf333c7a9ac773f011aafaa9b4830f5b853d2
Denisa <denisa.loseva@gmail.com>
2022-11-23 19:06:01 +0200

    Updated hw*

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,29 +15,55 @@ import java.util.List;
 
 public class FirstSeleniumTest {
     private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
-    private final By COUNTRIES = By.xpath(".//a[contains(@href, 'https://www.discovercars.com')]");
+   // private final By COUNTRIES = By.xpath(".//a[contains(@href, 'https://www.discovercars.com')]");
     private final By TITLE = By.xpath(".//h1[contains(@class, 'home-h1')]");
+
+    private final By TOP_LOCATION_LINK = By.xpath(".//div[@class = 'tl-box']/a");
+
+    private WebDriver browser;
+    private WebDriverWait wait;
+
 
     @Test
     public void openHomePageCheck() {
+        String country = "Latvia";
+
         System.setProperty("webdriver.chrome.driver", "D://chromedriver.exe");
-        WebDriver browser = new ChromeDriver();
+        browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.get("http://www.discovercars.com/");
 
-        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
+        wait = new WebDriverWait(browser, Duration.ofSeconds(10));
 
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
         browser.findElement(ACCEPT_COOKIES_BTN).click();
 
-        List<WebElement> countries = browser.findElements(COUNTRIES);
-        wait.until(ExpectedConditions.elementToBeClickable(countries.get(251)));
-        countries.get(251).click();
+        clickOnCountry(country);
+
+      //  List<WebElement> countries = browser.findElements(COUNTRIES);
+       // wait.until(ExpectedConditions.elementToBeClickable(countries.get(251)));
+       // countries.get(251).click();
+
 
         browser.findElement(TITLE);
         System.out.println(browser.findElement(TITLE).getText());
 
-        browser.quit();
+        // browser.quit();
+    }
+    private void clickOnCountry(String country) {
+        List<WebElement> links = browser.findElements(TOP_LOCATION_LINK);
+
+        boolean isCountryFound = false;
+        for (WebElement link : links) {
+            if (link.getText().equals(country)) {
+                isCountryFound = true;
+                wait.until(ExpectedConditions.elementToBeClickable(link));
+                // link.findElement(By.tagName("a")).click();
+                link.click();
+                break;
+            }
+        }
+        Assertions.assertTrue(isCountryFound, "Country not found");
     }
 }
 
